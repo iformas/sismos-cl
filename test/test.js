ae086a6f147bb1f38c307a4350cbc8a3538c1797
iformas <iformas@hotmail.com>
2017-02-07 15:29:51 -0300

    fixed test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
 const should = require('chai').should();
-const expect = require('chai').expect();
+const expect = require('chai').expect;
 const sismos = require('../index');
-const _ = require('lodash')
 
 describe('#test', function () {
   it('Verifiaca que el módulo funcione de forma correcta', function () {
     expect(function () {
-      sismos().then(function (sismos) {});
+      sismos().then(function (sismos) {
+      console.log(sismos);
+      });
     }).to.not.throw(Error);
   });
 });
